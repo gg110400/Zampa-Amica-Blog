a43b32416d2def68d5530458b2f74bdf43fae331
gg110402 <giuliagiudici110402@gmail.com>
2024-09-03 12:21:29 +0200

    generazione nuovo token quando il vecchio scade

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,26 +4,59 @@ import { UnauthorizedError } from '../utils/errorTypes.js';
 
 const authMiddleware = async (req, res, next) => {
   try {
+    // Estrai il token dall'intestazione Authorization
     const token = req.header('Authorization')?.replace('Bearer ', '');
     if (!token) {
-      throw new UnauthorizedError('No token provided');
+      throw new UnauthorizedError('Nessun token fornito');
     }
     
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.user.id);
+    let decoded;
+    try {
+      // Verifica il token
+      decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+      if (error instanceof jwt.TokenExpiredError) {
+        // Il token è scaduto, genera un nuovo token
+        decoded = jwt.decode(token);
+        if (decoded && decoded.user && decoded.user.id) {
+          const user = await User.findById(decoded.user.id);
+          if (!user) {
+            throw new UnauthorizedError('Utente non trovato');
+          }
 
-    if (!user) {
-      throw new UnauthorizedError('User not found or token invalid');
+          // Genera un nuovo token
+          const newToken = jwt.sign({ user: { id: user._id } }, process.env.JWT_SECRET, {
+            expiresIn: '1h', // Imposta il tempo di scadenza desiderato
+          });
+
+          // Opzionalmente, imposta il nuovo token nelle intestazioni della risposta o invialo nel corpo della risposta
+          res.set('Authorization', `Bearer ${newToken}`);
+
+          // Imposta req.user e continua
+          req.user = user;
+          return next();
+        } else {
+          throw new UnauthorizedError('Token non valido');
+        }
+      } else if (error instanceof jwt.JsonWebTokenError) {
+        throw new UnauthorizedError('Token non valido');
+      } else {
+        throw error;
+      }
     }
 
+    // Cerca l'utente nel database usando l'ID dal token decodificato
+    const user = await User.findById(decoded.user.id);
+    if (!user) {
+      throw new UnauthorizedError('Utente non trovato o token non valido');
+    }
+
+    // Imposta l'utente su req.user e passa al middleware successivo
     req.user = user;
     next();
   } catch (error) {
-    if (error instanceof jwt.JsonWebTokenError) {
-      next(new UnauthorizedError('Invalid token'));
-    } else {
-      next(error);
-    }
+    // Gestione degli errori
+    next(error);
   }
 };
 
